9ca8aaae510b7479eff70c8ce630f83e5bc1635a
Greg Rosich <pyroscout14@gmail.com>
2017-02-17 12:03:22 -0700

    Beginning testing

---
+++ Spreadsheet/Spreadsheet/Cell.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SS
+{
+    /// <summary>
+    /// This class is a helper class for Spreadsheet. Each cell contains a name
+    /// and contents. The contents can be either a string, double, or a Formula
+    /// </summary>
+    class Cell
+    {
+        public string name { get; }
+        public object contents { get; }
+    
+        /// <summary>
+        /// Creates a cell object with the name and contents specified
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="contents"></param>
+        public Cell(string name, object contents)
+        {
+            this.name = name;
+            this.contents = contents;
+        }
+    }
+}
---
+++ Spreadsheet/SpreadsheetTests/SpreadsheetTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SS;
+using Formulas;
+
+namespace SpreadsheetTests
+{
+    [TestClass]
+    public class SpreadsheetTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void InvalidName1()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            ss.GetCellContents("a03");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void InvalidName2()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            ss.GetCellContents("hello");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void InvalidName3()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            ss.GetCellContents("15");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void InvalidName4()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            ss.GetCellContents("a3*");
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            Assert.AreEqual(ss.GetCellContents("a3"), "");
+        }
+        
+        [TestMethod]
+        public void Test2()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            ss.SetCellContents("a1", 10);
+        }
+    }
+}
