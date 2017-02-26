1e63ae4ce7d6208c61bad99ef412b11d9701316c
Greg Rosich <pyroscout14@gmail.com>
2017-02-25 20:31:45 -0700

    Testing new cell class

--- Spreadsheet/Spreadsheet/Cell.cs
+++ Spreadsheet/Spreadsheet/Cell.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Formulas;
 
 namespace SS
 {
@@ -12,18 +13,56 @@ namespace SS
     /// </summary>
     class Cell
     {
-        public string name { get; }
-        public object contents { get; }
-    
+        public string Name { get; private set; }
+        public object Contents { get
+            {
+                if (Text != null)
+                {
+                    return Text;
+                }
+                if (!Object.ReferenceEquals(formula, null))
+                {
+                    return formula;
+                }
+                return Value;
+            }
+        }
+        public double Value { get; private set; }
+        private string Text { get; set; }
+        private Formula formula;
+
+        private Cell(string name)
+        {
+            this.Name = name;
+        }
         /// <summary>
-        /// Creates a cell object with the name and contents specified
+        /// Creates a cell object with the name and value specified
         /// </summary>
         /// <param name="name"></param>
-        /// <param name="contents"></param>
-        public Cell(string name, object contents)
+        /// <param name="value"></param>
+        public Cell(string name, double value) : this(name)
         {
-            this.name = name;
-            this.contents = contents;
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Creates a cell object with the name and formula specified
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="formula"></param>
+        public Cell(string name, Formula formula) : this(name)
+        {
+            this.formula = formula;
+        }
+
+        /// <summary>
+        /// Creates a cell object with the name and text specified
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="text"></param>
+        public Cell(string name, string text) : this(name)
+        {
+            this.Text = text;
         }
     }
 }
--- Spreadsheet/Spreadsheet/Spreadsheet.cs
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Formulas;
 using System.Text.RegularExpressions;
 using Dependencies;
+using System.IO;
 
 namespace SS
 {
@@ -56,6 +57,23 @@ namespace SS
         Dictionary<string, Cell> cells;
 
         /// <summary>
+        /// True if this spreadsheet has been modified since it was created or saved
+        /// (whichever happened most recently); false otherwise.
+        /// </summary>
+        public override bool Changed
+        {
+            get
+            {
+                return Changed;
+            }
+
+            protected set
+            {
+                Changed = value;
+            }
+        }
+
+        /// <summary>
         /// Creats a new empty spreadsheet
         /// </summary>
         public Spreadsheet()
@@ -83,7 +101,7 @@ namespace SS
             {
                 return "";
             }
-            return cells[name].contents;
+            return cells[name].Contents;
         }
 
         /// <summary>
@@ -110,7 +128,7 @@ namespace SS
         /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
         /// set {A1, B1, C1} is returned.
         /// </summary>
-        public override ISet<string> SetCellContents(string name, Formula formula)
+        protected override ISet<string> SetCellContents(string name, Formula formula)
         {
             if (name == null || !ValidateName(name))
             {
@@ -138,7 +156,7 @@ namespace SS
         /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
         /// set {A1, B1, C1} is returned.
         /// </summary>
-        public override ISet<string> SetCellContents(string name, string text)
+        protected override ISet<string> SetCellContents(string name, string text)
         {
             if(text == null)
             {
@@ -173,7 +191,7 @@ namespace SS
         /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
         /// set {A1, B1, C1} is returned.
         /// </summary>
-        public override ISet<string> SetCellContents(string name, double number)
+        protected override ISet<string> SetCellContents(string name, double number)
         {
             if (name == null || !ValidateName(name))
             {
@@ -240,6 +258,107 @@ namespace SS
                 set.Add(dependent);
             }
             return set;
+        }
+
+        /// <summary>
+        /// Writes the contents of this spreadsheet to dest using an XML format.
+        /// The XML elements should be structured as follows:
+        ///
+        /// <spreadsheet IsValid="IsValid regex goes here">
+        ///   <cell name="cell name goes here" contents="cell contents go here"></cell>
+        ///   <cell name="cell name goes here" contents="cell contents go here"></cell>
+        ///   <cell name="cell name goes here" contents="cell contents go here"></cell>
+        /// </spreadsheet>
+        ///
+        /// The value of the IsValid attribute should be IsValid.ToString()
+        /// 
+        /// There should be one cell element for each non-empty cell in the spreadsheet.
+        /// If the cell contains a string, the string (without surrounding double quotes) should be written as the contents.
+        /// If the cell contains a double d, d.ToString() should be written as the contents.
+        /// If the cell contains a Formula f, f.ToString() with "=" prepended should be written as the contents.
+        ///
+        /// If there are any problems writing to dest, the method should throw an IOException.
+        /// </summary>
+        public override void Save(TextWriter dest)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// If name is null or invalid, throws an InvalidNameException.
+        ///
+        /// Otherwise, returns the value (as opposed to the contents) of the named cell.  The return
+        /// value should be either a string, a double, or a FormulaError.
+        /// </summary>
+        public override object GetCellValue(string name)
+        {
+            if (name == null || !ValidateName(name))
+            {
+                throw new InvalidNameException();
+            }
+
+            if (!cells.ContainsKey(name))
+            {
+                return "";
+            }
+            return cells[name].Value;
+        }
+
+        /// <summary>
+        /// If content is null, throws an ArgumentNullException.
+        ///
+        /// Otherwise, if name is null or invalid, throws an InvalidNameException.
+        ///
+        /// Otherwise, if content parses as a double, the contents of the named
+        /// cell becomes that double.
+        ///
+        /// Otherwise, if content begins with the character '=', an attempt is made
+        /// to parse the remainder of content into a Formula f using the Formula
+        /// constructor with s => s.ToUpper() as the normalizer and a validator that
+        /// checks that s is a valid cell name as defined in the AbstractSpreadsheet
+        /// class comment.  There are then three possibilities:
+        ///
+        ///   (1) If the remainder of content cannot be parsed into a Formula, a
+        ///       Formulas.FormulaFormatException is thrown.
+        ///
+        ///   (2) Otherwise, if changing the contents of the named cell to be f
+        ///       would cause a circular dependency, a CircularException is thrown.
+        ///
+        ///   (3) Otherwise, the contents of the named cell becomes f.
+        ///
+        /// Otherwise, the contents of the named cell becomes content.
+        ///
+        /// If an exception is not thrown, the method returns a set consisting of
+        /// name plus the names of all other cells whose value depends, directly
+        /// or indirectly, on the named cell.
+        ///
+        /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
+        /// set {A1, B1, C1} is returned.
+        /// </summary>
+        public override ISet<string> SetContentsOfCell(string name, string content)
+        {
+            if (content == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (name == null || !ValidateName(name))
+            {
+                throw new InvalidNameException();
+            }
+
+            double value;
+            if(Double.TryParse(content, out value))
+            {
+                return SetCellContents(name, value);
+            }
+            string pattern = @"^=";
+            if (Regex.IsMatch(content, pattern))
+            {
+                Regex.Replace(content, pattern, "");
+                Formula f = new Formula(content, s => s.ToUpper(), s => ValidateName(s));
+                return SetCellContents(name, f);
+            }
+            return SetCellContents(name, content);
         }
     }
 }
