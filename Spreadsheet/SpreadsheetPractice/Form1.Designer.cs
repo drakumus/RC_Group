fa833e884d11c3d1710dc8a073558226ae5cfce2
Graham Cracker <Graham Cracker@GRIZ>
2017-03-07 21:23:51 -0700

    Half Way Point on impelmentation of spreadsheet.

---
+++ Spreadsheet/SpreadsheetPractice/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace SpreadsheetPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.spreadsheetPanel1 = new SSGui.SpreadsheetPanel();
+            this.cellLabel = new System.Windows.Forms.Label();
+            this.cellBox = new System.Windows.Forms.TextBox();
+            this.valueLable = new System.Windows.Forms.Label();
+            this.valueBox = new System.Windows.Forms.TextBox();
+            this.contentsLabel = new System.Windows.Forms.Label();
+            this.contentsBox = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // spreadsheetPanel1
+            // 
+            this.spreadsheetPanel1.Location = new System.Drawing.Point(0, 74);
+            this.spreadsheetPanel1.Name = "spreadsheetPanel1";
+            this.spreadsheetPanel1.Size = new System.Drawing.Size(803, 453);
+            this.spreadsheetPanel1.TabIndex = 0;
+            // 
+            // cellLabel
+            // 
+            this.cellLabel.AutoSize = true;
+            this.cellLabel.Location = new System.Drawing.Point(12, 49);
+            this.cellLabel.Name = "cellLabel";
+            this.cellLabel.Size = new System.Drawing.Size(27, 13);
+            this.cellLabel.TabIndex = 1;
+            this.cellLabel.Text = "Cell:";
+            // 
+            // cellBox
+            // 
+            this.cellBox.Location = new System.Drawing.Point(42, 46);
+            this.cellBox.Name = "cellBox";
+            this.cellBox.ReadOnly = true;
+            this.cellBox.Size = new System.Drawing.Size(100, 20);
+            this.cellBox.TabIndex = 2;
+            // 
+            // valueLable
+            // 
+            this.valueLable.AutoSize = true;
+            this.valueLable.Location = new System.Drawing.Point(148, 49);
+            this.valueLable.Name = "valueLable";
+            this.valueLable.Size = new System.Drawing.Size(37, 13);
+            this.valueLable.TabIndex = 3;
+            this.valueLable.Text = "Value:";
+            // 
+            // valueBox
+            // 
+            this.valueBox.Location = new System.Drawing.Point(191, 46);
+            this.valueBox.Name = "valueBox";
+            this.valueBox.ReadOnly = true;
+            this.valueBox.Size = new System.Drawing.Size(159, 20);
+            this.valueBox.TabIndex = 4;
+            // 
+            // contentsLabel
+            // 
+            this.contentsLabel.AutoSize = true;
+            this.contentsLabel.Location = new System.Drawing.Point(356, 49);
+            this.contentsLabel.Name = "contentsLabel";
+            this.contentsLabel.Size = new System.Drawing.Size(52, 13);
+            this.contentsLabel.TabIndex = 5;
+            this.contentsLabel.Text = "Contents:";
+            // 
+            // contentsBox
+            // 
+            this.contentsBox.Location = new System.Drawing.Point(414, 46);
+            this.contentsBox.Name = "contentsBox";
+            this.contentsBox.Size = new System.Drawing.Size(359, 20);
+            this.contentsBox.TabIndex = 6;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(804, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.newToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 528);
+            this.Controls.Add(this.contentsBox);
+            this.Controls.Add(this.contentsLabel);
+            this.Controls.Add(this.valueBox);
+            this.Controls.Add(this.valueLable);
+            this.Controls.Add(this.cellBox);
+            this.Controls.Add(this.cellLabel);
+            this.Controls.Add(this.spreadsheetPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private SSGui.SpreadsheetPanel spreadsheetPanel1;
+        private System.Windows.Forms.Label cellLabel;
+        private System.Windows.Forms.TextBox cellBox;
+        private System.Windows.Forms.Label valueLable;
+        private System.Windows.Forms.TextBox valueBox;
+        private System.Windows.Forms.Label contentsLabel;
+        private System.Windows.Forms.TextBox contentsBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+    }
+}
+
---
+++ Spreadsheet/SpreadsheetPractice/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SS;
+using SSGui;
+using Formulas;
+using System.IO;
+
+namespace SpreadsheetPractice
+{
+    public partial class Form1 : Form
+    {
+        Spreadsheet sheet;
+        public Form1()
+        {
+            InitializeComponent();
+            sheet = new Spreadsheet();
+            spreadsheetPanel1.SelectionChanged += displaySelection;
+            spreadsheetPanel1.SetSelection(2, 3);
+        }
+
+        private void displaySelection(SpreadsheetPanel ss)
+        {
+            int row, col;
+            String value;
+            ss.GetSelection(out col, out row);
+            ss.GetValue(col, row, out value);
+
+            string cell = translateCell(row, col);
+
+            cellBox.Text = cell;
+            valueBox.Text = value;
+            contentsBox.Text = sheet.GetCellContents(cell).ToString();
+        }
+
+        private string translateCell(int row, int col)
+        {
+            int rowInt = row + 1;
+            string colString = "";
+            switch (col)
+            {
+                case 0:
+                    colString = "A";
+                    break;
+                case 1:
+                    colString = "B";
+                    break;
+                case 2:
+                    colString = "C";
+                    break;
+                case 3:
+                    colString = "D";
+                    break;
+                case 4:
+                    colString = "E";
+                    break;
+                case 5:
+                    colString = "F";
+                    break;
+                case 6:
+                    colString = "G";
+                    break;
+                case 7:
+                    colString = "H";
+                    break;
+                case 8:
+                    colString = "I";
+                    break;
+                case 9:
+                    colString = "J";
+                    break;
+                case 10:
+                    colString = "K";
+                    break;
+                case 11:
+                    colString = "L";
+                    break;
+                case 12:
+                    colString = "M";
+                    break;
+                case 13:
+                    colString = "N";
+                    break;
+                case 14:
+                    colString = "O";
+                    break;
+                case 15:
+                    colString = "P";
+                    break;
+                case 16:
+                    colString = "Q";
+                    break;
+                case 17:
+                    colString = "R";
+                    break;
+                case 18:
+                    colString = "S";
+                    break;
+                case 19:
+                    colString = "T";
+                    break;
+                case 20:
+                    colString = "U";
+                    break;
+                case 21:
+                    colString = "V";
+                    break;
+                case 22:
+                    colString = "W";
+                    break;
+                case 23:
+                    colString = "X";
+                    break;
+                case 24:
+                    colString = "Y";
+                    break;
+                case 25:
+                    colString = "Z";
+                    break;
+            }
+            return colString + rowInt.ToString();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var desktopFolder = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            var fullFileName = Path.Combine(desktopFolder, "Test.xml");
+            var fs = new FileStream(fullFileName, FileMode.Create);
+            sheet.Save(new StreamWriter(fs));
+        }
+    }
+}
