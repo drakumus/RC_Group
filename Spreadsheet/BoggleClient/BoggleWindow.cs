dafc91c2341c5d61607cca7ec313baa52eff80f3
drakumus <rohancheeniyil@gmail.com>
2017-03-24 16:20:45 -0600

    Fix display on buttons

--- Spreadsheet/BoggleClient/BoggleWindow.cs
+++ Spreadsheet/BoggleClient/BoggleWindow.cs
@@ -19,7 +19,7 @@ namespace BoggleClient
         public BoggleWindow()
         {
             InitializeComponent();
-            buttons = new Button[] { button1, button2, button3, button4, button5, button6, button7, button8, button9, button10,
+            buttons = new Button[] { button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11,
             button12, button13, button14, button15, button16};
             wordList = new List<string>();
         }
