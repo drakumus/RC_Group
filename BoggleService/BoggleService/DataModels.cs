712df8d93b1568a2620c58cf54e74ec18bf484e6
drakumus <rohancheeniyil@gmail.com>
2017-03-30 19:24:48 -0600

    Class Setup

---
+++ BoggleService/BoggleService/DataModels.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Boggle
+{
+    public class PlayerInfo
+    {
+        public string Nickname { get; set; }
+        public string UserToken { get; set; }
+        public int Score;
+        List<WordItem> WordsPlayed;
+    }
+
+    public class Game
+    {
+        public string Board { get; set; }
+
+        public string TimeLimit { get; set; }
+        public int TimeLeft { get; set; }
+        public string GameState { get; set; }
+
+        public PlayerInfo Player1Info { get; set; }
+        public PlayerInfo Player2Info { get; set; }
+    }
+
+    public class WordItem
+    {
+        public string Word { get; set; }
+        public string Score { get; set; }
+    }
+}
