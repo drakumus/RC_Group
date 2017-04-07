32137a8acf4bb65cd37d0ee72e7b72ba64912c40
Greg Rosich <pyroscout14@gmail.com>
2017-04-07 02:13:12 -0600

    Wow

--- BoggleService/BoggleService/IBoggleService.cs
+++ BoggleService/BoggleService/IBoggleService.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
+using System.Text;
 
 namespace Boggle
 {
@@ -73,5 +77,36 @@ namespace Boggle
         /// </summary>
         [WebGet(UriTemplate = "/api")]
         Stream API();
+
+
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
     }
 }
+
