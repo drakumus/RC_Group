bbec1eb79e50f3f5ba3a8a41c4dc760d4b768577
Greg Rosich <pyroscout14@gmail.com>
2017-04-17 14:08:00 -0600

    Setup

---
+++ BoggleService/MyBoggleService/BoggleServer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boggle
+{
+    class BoggleServer
+    {
+        // Listens for incoming connection requests
+        private TcpListener server;
+
+        /// <summary>
+        /// Creates a BoggleServer that listens for connection requests on the specified port
+        /// </summary>
+        /// <param name="port"></param>
+        public BoggleServer(int port)
+        {
+            // A TcpListener listens for incoming connection requests
+            server = new TcpListener(IPAddress.Any, port);
+            
+            // Start the TcpListener
+            server.Start();
+
+            // Ask the server to call ConnectionRequested at some point in the future when 
+            // a connection request arrives.  It could be a very long time until this happens.
+            // The waiting and the calling will happen on another thread.  BeginAcceptSocket 
+            // returns immediately, and the constructor returns to Main.
+            server.BeginAcceptSocket(ConnectionRequested, null);
+        }
+
+        /// <summary>
+        /// This is the callback method that is passed to BeginAcceptSocket.  It is called
+        /// when a connection request has arrived at the server.
+        /// </summary>
+        /// <param name="result"></param>
+        private void ConnectionRequested(IAsyncResult result)
+        {
+            // We obtain the socket corresonding to the connection request.  Notice that we
+            // are passing back the IAsyncResult object.
+            Socket s = server.EndAcceptSocket(result);
+
+            // We ask the server to listen for another connection request.  As before, this
+            // will happen on another thread.
+            server.BeginAcceptSocket(ConnectionRequested, null);
+
+            // We create a new ClientConnection, which will take care of communicating with
+            // the remote client.
+            new BoggleConnection(s);
+        }
+    }
+}
--- BoggleService/MyBoggleService/Program.cs
+++ BoggleService/MyBoggleService/Program.cs
@@ -8,9 +8,14 @@ namespace Boggle
 {
     class Program
     {
+        /// <summary>
+        /// Launches a BoggleServer on port 60000.  Keeps the main
+        /// thread active so we can send output to the console.
+        /// </summary>
         static void Main(string[] args)
         {
-               
+            new BoggleServer(60000);
+            Console.ReadLine();
         }
     }
 }
